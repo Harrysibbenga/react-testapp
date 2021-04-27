3af85a71fdc864763759926b23bf041545799b4a
Harry Sibbenga <sibbengaharry@gmail.com>
2021-04-27 20:58:10 +0100

    React initial build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Main(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Harry',
+    avatarUrl: 'https://image.shutterstock.com/z/stock-photo-software-source-code-programming-code-programming-code-on-computer-screen-developer-working-on-634574354.jpg',
+  },
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Comment
+          date={comment.date}
+          text={comment.text}
+          author={comment.author} >
+        </Comment>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Main name="Harry" />
+          Welcome, is this your first time using React ?
         </p>
         <a
           className="App-link"
